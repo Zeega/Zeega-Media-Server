9bae06f2e64e8fe882f262ed93039ce1cd7697e0
Luis Brandao <luisbrandao@gmail.com>
2013-03-06 05:11:13 +0000

    Added ImagickService barebones

---
+++ src/Zeega/ImagickService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Zeega;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class ImagickService
+{
+    public function isAnimatedImage ( $image ) {
+        $frame = 0;
+        foreach($image->deconstructImages() as $i) {
+            $frame++;
+            if ($frame > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function coalesceIfAnimated ( $image ) {
+        if (self::isAnimatedImage($image)) {
+            return $image->coalesceImages();
+        }
+
+        return $image;
+    }
+    
+}
